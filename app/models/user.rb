0fed985c9a0dbe344c0d8610532255f1b3667af9
Jeremy Pastika <jpastika@spokesystems.com>
2013-03-18 09:20:42 -0400

    Working on import from CSV

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   #validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: { on: create }, length: { minimum: 8, if: password_digest_changed? }
+  validates :password, presence: { on: create }, length: { minimum: 6}, :if => :password_digest_changed?
   # validates :account_key, presence: true
   #validates :password_confirmation, presence: true
   
